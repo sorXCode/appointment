8509f448bb5b071f96c7e99e9282b13b99d3f769
sorxcode <sorxcode@gmail.com>
2021-01-28 12:05:46 +0100

    completed onboarding process

--- hospice/settings.py
+++ hospice/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -98,6 +102,27 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+}
+DJOSER = {
+    # other djoser settings
+    'SERIALIZERS': {
+         #other serializers
+
+         'user_create': 'users.serializers.CustomUserCreateSerializer',
+
+         #other serializers
+    }
+}
 
 
 # Internationalization
